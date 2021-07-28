2ef39700f954020d8758be92b424742f0da166d8
Kris <kristof.solak@gmail.com>
2021-07-28 17:01:44 +0200

    Password encoding added

--- src/main/java/com/shipper/service/CustomUserService.java
+++ src/main/java/com/shipper/service/CustomUserService.java
@@ -1,5 +1,10 @@
 package com.shipper.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.shipper.domain.Authority;
 import com.shipper.domain.User;
 import com.shipper.repository.UserDetailsRepository;
 import com.shipper.responses.UserRegister;
@@ -18,8 +24,11 @@ public class CustomUserService implements UserDetailsService {
 	
 	@Autowired
 	UserDetailsRepository userDetailsRepository;
-
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	List<Authority> authorityList;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -31,17 +40,57 @@ public class CustomUserService implements UserDetailsService {
 		return user;
 	}
 	
-	public String createUser(UserRegister userToRegister) {
-		
+	private Authority setAuthority(String roleCode,String roleDescription) {
+		Authority authority=new Authority();
+		authority.setRole(roleCode);
+		authority.setRoleDescription(roleDescription);
+		return authority;
+	}
+	public Boolean createUser(UserRegister userToRegister) {
+		boolean result = false; 
+		String role = "USER";
+		String role2 = "user role";
 		if (userDetailsRepository.findByUserName(userToRegister.getUserName())== null) {
+			authorityList=new ArrayList<>();
+			authorityList.add(setAuthority(role,role2));
 			User user = new User();
 			user.setFirstName(userToRegister.getUserName());
 			user.setEmail(userToRegister.getEmail());
-			user.setPassword(userToRegister.getPassword());
+			user.setUserName(userToRegister.getUserName());
+			user.setPassword(passwordEncoder.encode(userToRegister.getPassword()));
+			user.setEnabled(true);
+			user.setAuthorities(authorityList);
 			userDetailsRepository.save(user);
+			result = true;
 		}
 		
-		return "sikerult";
+		return result;
+	}
+	
+	@PostConstruct
+	protected void init() {
+		createUser("admin", "ADMIN", "Admin role", "admin");
+		createUser("user", "USER", "user role", "user");
+	}
+	
+	private void createUser(String userName, String role, String role2, String password) {
+		List<Authority> authorityList=new ArrayList<>();
+		authorityList.add(createAuthority(role,role2));
+		User user=new User();
+		user.setUserName(userName);
+		user.setFirstName(userName);
+		user.setPassword(passwordEncoder.encode(password));
+		user.setEnabled(true);
+		user.setAuthorities(authorityList);
+		userDetailsRepository.save(user);
+		
+	}
+	
+	private Authority createAuthority(String roleCode,String roleDescription) {
+		Authority authority=new Authority();
+		authority.setRole(roleCode);
+		authority.setRoleDescription(roleDescription);
+		return authority;
 	}
 
 }
